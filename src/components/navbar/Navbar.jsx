24911bf119687581f2f6bb638892c6ad68e9b133
Hemanya09 <hemanya1721.be21@chitkara.edu.in>
2024-03-21 00:45:00 +0530

    commit new things

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
             <Sidebar/>
             <div className="wrapper">
                 <motion.span initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.5}}>
-                    Career Connect
+                  <a href="https://career-connect-virid.vercel.app/"> Career Connect</a> 
                     </motion.span>
                 <div className="social">
                     <a href="#"><img src="/facebook.png" alt=""/></a>
-                    <a href="#"><img src="/instagram.png" alt=""/></a>
+                    <a href="https://www.instagram.com/careerconnectofficial?igsh=MnVrdXZtNWE3OGhu"><img src="/instagram.png" alt=""/></a>
                     <a href="#"><img src="/twitter.png" alt=""/></a>
                     <a href="#"><img src="/LinkedIn.png" alt=""/></a>
                 </div>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -42,25 +42,25 @@ const Services=()=>{
             <motion.div className="listContainer" variants={variants}>
                 <motion.div className="box" whileHover=
                 {{background:"lightgray",color:"black"}}>
-                 <h2>Communication Skills Enhancement</h2>
+                 <h3>Communication Skills Enhancement</h3>
                  <p> Strong communication allows us to express ourselves clearly, listen attentively to others, and build positive relationships.  Whether its collaborating with colleagues, negotiating a deal,  refined communication is key to success.  By enhancing these skills, we can navigate complex situations,  and achieve our goals.</p> 
                  {/* <button>Go</button>   */}
                 </motion.div>
                 <motion.div className="box" whileHover=
                 {{background:"lightgray",color:"black"}}>
-                 <h2>Workplace Etiquittes</h2>
+                 <h3>Workplace Etiquittes</h3>
                  <p>Workplace etiquette is like the unspoken language that keeps everything running smoothly. It fosters a respectful and professional environment where everyone feels comfortable contributing their best work.This positive atmosphere leads to better collaboration,  and ultimately, a more successful workplace for everyone.</p> 
                  {/* <button>Go</button>   */}
                 </motion.div>
                 <motion.div className="box" whileHover=
                 {{background:"lightgray",color:"black"}}>
-                 <h2>Personality Development</h2>
+                 <h3>Personality Development</h3>
                  <p>Personality development is like investing in your own happiness and success. Its a journey of self-discovery that helps you understand your strengths and weaknesses, build confidence, and cultivate positive traits. This translates into stronger relationships, both personal and professional.</p> 
                  {/* <button>Go</button>   */}
                 </motion.div>
                 <motion.div className="box" whileHover=
                 {{background:"lightgray",color:"black"}}>
-                 <h2>GDPI Prepration</h2>
+                 <h3>GDPI Prepration</h3>
                  <p>GDPI preparation acts as a launchpad for your dream B-school.  These Group Discussions and Personal Interviews are your chance to stand out from a pool of qualified candidates.  By honing your communication skills, critical thinking, and teamwork abilities in GDs, you showcase your potential to thrive in collaborative environments.</p> 
                  {/* <button>Go</button>   */}
                 </motion.div>
